37af5af7fedcf1b265d413dc307804b34d2b56fb
dungxcharles <nguyentiendungok@gmail.com>
2026-03-27 23:00:38 +0700

    feat: implement shortest path tracking when bfs return true

--- BFS.c
+++ BFS.c
@@ -3,6 +3,10 @@
 #include <stdbool.h>
 #define MAX 200                     // In the range of BFS, maze should not be too large
 
+typedef struct Pair{
+    int x, y;
+}Pair;
+
 typedef struct Node{
     int x, y;
     struct Node* next;
@@ -16,6 +20,12 @@ int maze[MAX][MAX] = {0};
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
 
+Pair parent[MAX][MAX];              // For shortest path finding
+
+Pair makePair(int x, int y){
+    Pair newPair; newPair.x = x; newPair.y = y;
+    return newPair;
+}
 
 Node* makeNode(int x, int y){
     Node* p = malloc(sizeof(Node));
@@ -63,6 +73,7 @@ void init(){
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
             scanf("%d",&maze[i][j]);
+            parent[i][j] = makePair(-1, -1);
         }
     }
 
@@ -83,12 +94,34 @@ bool bfs(){
             int y = dequeueNode->y + dy[i];
             if (isValid(x, y)){
                 maze[x][y] = 1;
+                parent[x][y] = makePair(dequeueNode->x, dequeueNode->y);
                 if (x == xt && y == yt) return true;
                 enqueue(makeNode(x, y));
             }
         }
     }
     return false;
+}
+
+void printShortestPath(){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            maze[i][j] = 0;
+        }
+    }
+    int x = xt, y = yt;
+    while (x != -1 && y != -1){
+        maze[x][y] = 1;
+        int tx = parent[x][y].x;
+        int ty = parent[x][y].y;
+        x = tx; y = ty;
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            printf("%d ",maze[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main(){
@@ -100,10 +133,16 @@ int main(){
         return 0;
     }
     printf("BFS status: ");
-    if (bfs()){
+    bool checkFlag = bfs();
+    if (checkFlag){
         printf("TRUE\n");
     }else{
         printf("FALSE\n");
+    }
+
+    if (checkFlag){
+        printf("The shortest path:\n");
+        printShortestPath();
     }
     return 0;
 }
