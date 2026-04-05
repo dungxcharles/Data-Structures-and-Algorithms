3d0fc17a82254bd8df30db124dece61375ac8481
dungxcharles <nguyentiendungok@gmail.com>
2026-04-05 18:06:12 +0700

    feat: add A* algorithm file, constructor and readInput method

--- matrix/Shortest Path A_star/Astar.java
+++ matrix/Shortest Path A_star/Astar.java
@@ -1,5 +1,31 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
 public class Astar {
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    // Node info
+    private class Node{
+        int x, y;
+        int f, g, h;        // f = g + h
+        Node(int x, int y, int f, int g, int h){
+            this.x = x; this.y = y;
+            this.f = f; this.g = g; this.h = h;
+        }
+    }
+
+    // Literals
+    private int rows, cols;
+    private Node[][] mat;
+    private Node source, target;
+
+    public void readInput(){
+        Scanner sc = new Scanner(System.in);
+        rows = sc.nextInt();
+        cols = sc.nextInt();
+        mat = new Node[rows][cols];
+
+        int xs = sc.nextInt(), ys = sc.nextInt();
+        source = new Node(xs, ys, 0, 0, 0);
+        int xt = sc.nextInt(), yt = sc.nextInt();
+        target = new Node(xt, yt, 0, 0, 0);
     }
 }
